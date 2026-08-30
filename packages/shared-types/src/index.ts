b92cb5f6f1a52f272fabd4ae4634c2daccd640c6
Seyha Chim <32429415+seyhachim@users.noreply.github.com>
2026-08-30 08:01:29 +0700

    feat(resources): export resource contracts

--- packages/shared-types/src/index.ts
+++ packages/shared-types/src/index.ts
@@ -70,3 +70,4 @@ export * from "./action-research.ts";
 export * from "./community.ts";
 export * from "./community-plugin.ts";
 export * from "./student-handbook.ts";
+export * from "./resources.ts";
