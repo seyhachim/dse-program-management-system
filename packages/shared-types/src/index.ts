a3461bf125bdb626bad39bf23bf83aecdb75cd6c
Seyha Chim <32429415+seyhachim@users.noreply.github.com>
2026-09-10 19:49:28 +0700

    feat(offerings): export activation exception contracts (#963)

--- packages/shared-types/src/index.ts
+++ packages/shared-types/src/index.ts
@@ -23,6 +23,7 @@ export * from "./lecturer-portfolio-plugin.ts";
 export * from "./courses.ts";
 export * from "./offerings.ts";
 export * from "./offering-curriculum.ts";
+export * from "./offering-activation-exceptions.ts";
 export * from "./dashboard.ts";
 export * from "./attendance.ts";
 export * from "./class-responsibilities.ts";
